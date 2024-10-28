8ac8460c4529eee173e736db29b2fdcc9ba83b9a
DemidAntipin <antipindemid@inbox.ru>
2024-10-29 00:22:17 +0800

    rewrited pub.py to arduino-esp8266

---
+++ LedPhotoMqtt/sensor/Config.h
@@ -0,0 +1,12 @@
+String ssidAP = "ESP8266-DEMID"; // имя контроллера и точки доступа
+String passwordAP = "GUESS-THE-PASSWORD"; // пароль точки доступа
+
+const char* ssidCLI = "realme 9 Pro 5G"; // имя контроллера и точки доступа
+const char* passwordCLI = "12345678545"; // пароль точки доступа
+
+//char* ssidCLI = "";
+//char* passwordCLI = "";
+
+const char* mqtt_broker = "broker.emqx.io";
+
+const int mqtt_port = 1883;
---
+++ LedPhotoMqtt/sensor/mqtt_sensor_publisher.ino
@@ -0,0 +1,140 @@
+#include "Config.h"
+#include "WIFI.h"
+#include "MQTT.h"
+
+#define PHOTO_RESISTOR A0
+#define READ_RESISTOR 'p'
+#define SWITCH_STREAM 's'
+
+int p_val;
+bool stream_mode = false;
+bool pub_id_sended = false;
+
+long int instant = 0;
+long int average = 0;
+long int stream = 0;
+long int stream_start = 0;
+long int min_max = 0;
+int mi = 256;
+int ma = 0;
+
+String pub_id;
+String topic_get_pub_id;
+String topic_confirm;
+String topic_photo_min;
+String topic_photo_max;
+String topic_photo_instant;
+String topic_photo_average;
+String topic_photo_stream;
+String topic_active_stream;
+
+int arr[100] = {NAN};
+long arrive_time[100];
+
+void clean_messages() {
+    long current = millis();
+    int to_delete = 0;
+    for (int i = 0; i < 100; i++) {
+        if (isnan(arr[i])) {
+            continue;
+        }
+        if (current - arrive_time[i] >= 2000) {  // 2 секунды
+            to_delete++;
+        } else {
+            break;
+        }
+    }
+    for (int i = 0; i < 100 - to_delete; i++) {
+        arr[i] = arr[i + to_delete];
+        arrive_time[i] = arrive_time[i + to_delete];
+    }
+    for (int i = 100 - to_delete; i < 100; i++) {
+        arr[i] = NAN;
+        arrive_time[i] = 0;
+    }
+}
+
+void setup() {
+  Serial.begin(115200);
+  WIFI_init(false);
+  MQTT_init();
+  pub_id = id();
+  topic_get_pub_id = "lab/" + pub_id + "/get_pub_id";
+  topic_confirm = "lab/" + pub_id + "/confirm";
+  topic_photo_min = "lab/" + pub_id + "/photo/min";
+  topic_photo_max = "lab/" + pub_id + "/photo/max";
+  topic_photo_instant = "lab/" + pub_id + "/photo/instant";
+  topic_photo_average = "lab/" + pub_id + "/photo/average";
+  topic_photo_stream = "lab/" + pub_id + "/photo/stream";
+  topic_active_stream = "lab/" + pub_id + "/photo/active_stream";
+  mqtt_cli.setCallback(callback);
+  mqtt_cli.subscribe(topic_confirm.c_str());
+  while (!pub_id_sended) {
+    Serial.print("Trying to send pub_id to subscriber");
+    mqtt_cli.publish(topic_get_pub_id.c_str(), pub_id.c_str());
+    delay(500);
+    mqtt_cli.loop();
+  }
+}
+
+float calculate_average() {
+    float sum = 0;
+    int count = 0;
+    for (int i = 0; i < 100; i++) {
+        if (!isnan(arr[i])) {
+            sum += arr[i];
+            count++;
+        }
+    }
+    return count > 0 ? sum / count : NAN;
+}
+
+void loop() {
+    clean_messages();
+    p_val = analogRead(PHOTO_RESISTOR);
+    memmove(arr, arr + 1, (99) * sizeof(int));
+    arr[99] = p_val;
+    memmove(arrive_time, arrive_time + 1, (99) * sizeof(long));
+    arrive_time[99] = millis();
+    long current = millis();
+    
+    if (current - min_max >= 5000) {
+        Serial.print("Publish min: " + String(mi));
+        mqtt_cli.publish(topic_photo_min.c_str(), String(mi).c_str());
+        Serial.print("Publish max: " + String(ma));
+        mqtt_cli.publish(topic_photo_max.c_str(), String(ma).c_str());
+        min_max = millis();
+    }
+    
+    if (current - instant >= 1000) {
+        Serial.print("Publish instant: " + String(arr[99]));
+        mqtt_cli.publish(topic_photo_instant.c_str(), String(arr[99]).c_str());
+        instant = millis();
+    }
+    
+    if (current - average >= 1000) {
+        float avg = calculate_average();
+        Serial.print("Publish average: " + String(avg));
+        mqtt_cli.publish(topic_photo_average.c_str(), String(avg).c_str());
+        average = millis();
+    }
+    
+    if (stream_mode && current - stream >= 100) {
+        Serial.print("Publish stream: " + String(arr[99]));
+        mqtt_cli.publish(topic_photo_stream.c_str(), String(arr[99]).c_str());
+        stream = millis();
+    }
+    
+    if (current - stream_start > 10000) {
+        if (stream_mode) {
+            Serial.print("Publish active_stream: off\n");
+            mqtt_cli.publish(topic_active_stream.c_str(), "off");
+            stream_mode = false;
+        } else {
+            Serial.print("Publish active_stream: on\n");
+            mqtt_cli.publish(topic_active_stream.c_str(), "on");
+            stream_mode = true;
+        }
+        stream_start = millis();
+    }
+}
